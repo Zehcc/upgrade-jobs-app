521319edf4a14440dc1e273df33dad97ea88ee95
Gonzalo Santos <gsanore@gmail.com>
2022-04-14 18:56:22 +0200

    logo created

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Logo from '../../components/Logo/Logo';
 import './_landingPage.scss';
 
 const LandingPage = () => {
-  return <div className='landing-page'>LandingPage</div>;
+  return (
+    <div className='landing-page'>
+      <Logo />
+    </div>
+  );
 };
 
 export default LandingPage;
