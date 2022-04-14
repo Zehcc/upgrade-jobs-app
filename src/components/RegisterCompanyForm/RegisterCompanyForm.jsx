54be16463b4ab1d4df53448f77efb3b48004a6bf
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-04-14 18:04:12 +0200

    chore: adding register company form content

--- src/components/RegisterCompanyForm/RegisterCompanyForm.jsx
+++ src/components/RegisterCompanyForm/RegisterCompanyForm.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {useForm} from 'reac-hook-form'
 
 const RegisterCompanyForm = () => {
+  const {register, handleSubmit} = useForm();
   return (
-    <div>RegisterCompanyForm</div>
+    <div>
+      <form >
+        <label>Nombre</label>
+        <input type="text" name="firstName" {...register("firstName", { required: true, minLength: 4 })} />
+        <label>Cif</label>
+        <input type="text" name="firstName" {...register("firstName", { required: true, minLength: 4 })} />
+        <label>Email</label>
+        <input type="text" name="firstName" {...register("firstName", { required: true, minLength: 4 })} />
+        <label>Contraseña</label>
+        <input type="text" name="firstName" {...register("firstName", { required: true, minLength: 4 })} />
+        
+      </form>
+    </div>
   )
 }
 
