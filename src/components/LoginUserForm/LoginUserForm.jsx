30cd949c60b7b14006de5e4e5a9d2c12e4860584
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-04-15 11:47:10 +0200

    chore: me equivoque

--- src/components/LoginUserForm/LoginUserForm.jsx
+++ src/components/LoginUserForm/LoginUserForm.jsx
@@ -2,15 +2,17 @@ import {useForm} from 'react-hook-form';
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {API} from '../../shared/services/api';
+import { useIsAuthUserContext } from '../../shared/contexts/IsAuthUserContext';
 
 const LoginUserForm = () => {
   const {register, handleSubmit} = useForm();
+  const {setIsAuthUser} = useIsAuthUserContext()
 
   const onSubmit = (data) => {
     API.post('/users/login', data).then((response) => {
       console.log(response);
-      /* localStorage.setItem('token', response.data)
-        setIsAuthCompany(response.data) */
+      localStorage.setItem('token', response.data[0])
+      setIsAuthUser(response.data[0])
     });
   };
 
