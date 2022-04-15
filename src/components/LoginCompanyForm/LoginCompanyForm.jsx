7cc5202297f53e48fe72b0026f99bbb55e5cd6cb
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-04-15 12:22:01 +0200

    chore: added profile context + profile on logins

--- src/components/LoginCompanyForm/LoginCompanyForm.jsx
+++ src/components/LoginCompanyForm/LoginCompanyForm.jsx
@@ -3,16 +3,34 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { API } from "../../shared/services/api";
 import { useIsAuthCompanyContext } from "../../shared/contexts/IsAuthCompanyContext";
+import { useProfileContext } from "../../shared/contexts/ProfileContext";
 
 const LoginCompanyForm = () => {
   const { register, handleSubmit } = useForm();
   const { setIsAuthCompany } = useIsAuthCompanyContext();
+  const {companyProfile, setCompanyProfile} = useProfileContext();
 
   const onSubmit = (data) => {
     API.post("/companies/login", data).then((response) => {
       console.log(response);
       localStorage.setItem('token', response.data[0])
       setIsAuthCompany(response.data[0])
+      setCompanyProfile(
+        {
+          id: response.data[1]._id,
+          name: response.data[1].name,
+          email: response.data[1].email,
+          cif: response.data[1].cif,
+          info: 
+              {
+                  description: response.data[1].description,
+                  img: response.data[1].img,
+                  location: response.data[1].location,
+                  web: response.data[1].web,
+                  employees: response.data[1].employees
+              }
+      })
+      console.log(companyProfile)
     });
   };
 
--- src/components/LoginUserForm/LoginUserForm.jsx
+++ src/components/LoginUserForm/LoginUserForm.jsx
@@ -3,17 +3,26 @@ import React from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import {API} from '../../shared/services/api';
 import {useIsAuthUserContext} from '../../shared/contexts/IsAuthUserContext';
+import { useProfileContext } from '../../shared/contexts/ProfileContext';
 
 const LoginUserForm = () => {
   const {register, handleSubmit} = useForm();
   let navigate = useNavigate();
   const {setIsAuthUser} = useIsAuthUserContext();
+  const {setUserProfile} = useProfileContext();
 
   const onSubmit = (data) => {
     API.post('/users/login', data).then((response) => {
       console.log(response);
       localStorage.setItem('token', response.data[0]);
       setIsAuthUser(response.data[0]);
+      setUserProfile(
+        { id: response.data[1]._id,
+          name: response.data[1].name,
+          email: response.data[1].email,
+          img: response.data[1].img
+        }
+      )
       navigate('/offers');
     });
   };
