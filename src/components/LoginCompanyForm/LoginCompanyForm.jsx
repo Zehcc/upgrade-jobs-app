3f14fc75cba8ce20cadd7ec4b4d8eae041196eaa
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-04-15 11:28:29 +0200

    chore: added login company content

--- src/components/LoginCompanyForm/LoginCompanyForm.jsx
+++ src/components/LoginCompanyForm/LoginCompanyForm.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { API } from "../../shared/services/api";
+import { useIsAuthCompanyContext } from "../../shared/contexts/IsAuthCompanyContext";
 
 const LoginCompanyForm = () => {
-  
+  const { register, handleSubmit } = useForm();
+  const { setIsAuthCompany } = useIsAuthCompanyContext();
+
+  const onSubmit = (data) => {
+    API.post("/companies/login", data).then((response) => {
+      console.log(response);
+      localStorage.setItem('token', response.data[0])
+      setIsAuthCompany(response.data[0])
+    });
+  };
+
   return (
-  <>
-  <div>Login company
-  </div>
-  <Link to="/registerCompany">
-    <button>Ir a registro</button>
-  </Link>
-  </>
-  )
+    <>
+      <div className="login-form">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>CIF</label>
+          <input
+            type="text"
+            name="cif"
+            {...register("cif", { require: true })}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            {...register("password", { require: true })}
+          />
+          <button>Login</button>
+        </form>
+      </div>
+      <Link to="/registerCompany">
+        <button>Ir a registro</button>
+      </Link>
+    </>
+  );
 };
 
 export default LoginCompanyForm;
