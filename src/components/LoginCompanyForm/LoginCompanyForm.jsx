7e4bd817f0b342aa0b79fd4ef49e025e865eb324
Gonzalo Santos <gsanore@gmail.com>
2022-04-18 17:02:34 +0200

    minor changes

--- src/components/LoginCompanyForm/LoginCompanyForm.jsx
+++ src/components/LoginCompanyForm/LoginCompanyForm.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { API } from "../../shared/services/api";
-import { useIsAuthCompanyContext } from "../../shared/contexts/IsAuthCompanyContext";
-import { useProfileContext } from "../../shared/contexts/ProfileContext";
+import React from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
+import {API} from '../../shared/services/api';
+import {useIsAuthCompanyContext} from '../../shared/contexts/IsAuthCompanyContext';
+import {useProfileContext} from '../../shared/contexts/ProfileContext';
 
 const LoginCompanyForm = () => {
-  const { register, handleSubmit } = useForm();
+  const {register, handleSubmit} = useForm();
   let navigate = useNavigate();
-  const { setIsAuthCompany } = useIsAuthCompanyContext();
-  const { companyProfile, setCompanyProfile } = useProfileContext();
+  const {setIsAuthCompany} = useIsAuthCompanyContext();
+  const {companyProfile, setCompanyProfile} = useProfileContext();
 
   const onSubmit = (data) => {
-    API.post("/companies/login", data).then((response) => {
-      localStorage.setItem("token", response.data[0]);
+    API.post('/companies/login', data).then((response) => {
+      localStorage.setItem('token', response.data[0]);
       setIsAuthCompany(response.data[0]);
       setCompanyProfile({
         id: response.data[1]._id,
@@ -26,36 +26,36 @@ const LoginCompanyForm = () => {
           location: response.data[1].info.location,
           web: response.data[1].info.web,
           employees: response.data[1].info.employees,
-        }
+        },
       });
-      navigate(`/companyProfile/${companyProfile.id}`)
+      navigate(`/companyProfile/${companyProfile.id}`);
       console.log(companyProfile);
     });
   };
 
   return (
     <>
-      <div className="login-form-container">
-        <form  onSubmit={handleSubmit(onSubmit)}>
+      <div className='login-form-container'>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <input
-            type="text"
-            name="cif"
-            placeholder="📝 Cif"
-            {...register("cif", { require: true })}
+            type='text'
+            name='cif'
+            placeholder='📝 Cif'
+            {...register('cif', {require: true})}
           />
           <input
-            type="password"
-            name="password"
-            placeholder="🔐 Password"
-            {...register("password", { require: true })}
+            type='password'
+            name='password'
+            placeholder='🔐 Password'
+            {...register('password', {require: true})}
           />
           <button>Sign in</button>
         </form>
       </div>
-      <div className="register-div">
+      <div className='register-div'>
         <p>¿Aún no estas registrado?</p>
-        <Link to="/registerCompany">
-          <button className="sign-up">Sign up</button>
+        <Link to='/registerCompany'>
+          <button className='sign-up'>Sign up</button>
         </Link>
       </div>
     </>
