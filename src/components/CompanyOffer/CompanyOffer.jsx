d7b0b5c03acb0039205296c79a50b0970bd5fdb1
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-04-19 12:15:02 +0200

    chore: trying to fix an issue on CompanyOffers

--- src/components/CompanyOffer/CompanyOffer.jsx
+++ src/components/CompanyOffer/CompanyOffer.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { useProfileContext } from "../../shared/contexts/ProfileContext";
 
 const CompanyOffer = ({ offer }) => {
-  const { companyProfile } = useProfileContext();
-  console.log(companyProfile);
   return (
     <li className="offer-container">
       <div className="offer-text-container">
         <h4>{offer.title}</h4>
         <p>Vacantes: {offer.vacants}</p>
-        {offer.candidates.length && <p>Candidatos: {offer.candidates.length}</p>}
+        <p>Candidatos: {offer.candidates.length}</p>
       </div>
     </li>
   );
