14db4d36aa08c8d1d136afed3ac4366dad2a0702
Ruben <ruben.infantes@gmail.com>
2022-04-26 16:53:59 +0200

    added dropdown button in navbar-company and fix companyiffers page

--- src/shared/components/CompanyNavbar/CompanyNavbar.jsx
+++ src/shared/components/CompanyNavbar/CompanyNavbar.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useProfileContext } from "../../contexts/ProfileContext";
+import ButtonLogout from "../../../components/ButtonLogout/ButtonLogout";
 
 const CompanyNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleButtonClick = () => {
+    setIsOpen(!isOpen);
+  };
   const { companyProfile } = useProfileContext();
   return (
     <nav className="company-navbar">
       <Link to="/companyOffers">
         <div className="nav-item">Mis ofertas</div>
       </Link>
-      <Link to={`/companyProfile/${companyProfile.id}`}>
-        <div className="nav-item">Perfil</div>
-      </Link>
+      <div className="dropdown">
+        <button className="dropbtn" type="button" onClick={handleButtonClick}>Configuración</button>
+        <div className={`dropdown-submenu ${isOpen ? "show" : ""}`} >
+          <ul>
+            <li>
+              <Link to={`/companyProfile/${companyProfile.id}`}>
+                <div className="nav-item">Perfil</div>
+              </Link>
+            </li>
+            <li>
+              <ButtonLogout className="nav-item" />
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
