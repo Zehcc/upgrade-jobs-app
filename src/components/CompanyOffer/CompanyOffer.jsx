a366bd23ad4588e00ec9b9f9d440498c1f6fb46f
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-05-03 10:46:32 +0200

    chore: added cv upload

--- src/components/CompanyOffer/CompanyOffer.jsx
+++ src/components/CompanyOffer/CompanyOffer.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const CompanyOffer = ({ offer }) => {
   return (
-    offer &&
-    <li className="offer-container">
-      <div className="offer-text-container">
-        <h4>{offer.title}</h4>
-        <p>Vacantes: {offer.vacants}</p>
-        <p>Candidatos: {offer.candidates.length}</p>
-      </div>
-    </li>
+    offer && (
+      <li className='offer-container'>
+        <div className='offer-text-container'>
+          <h4>{offer.title}</h4>
+          <p>Vacantes: {offer.vacants}</p>
+          <p>Candidatos: {offer.candidates.length}</p>
+        </div>
+      </li>
+    )
   );
 };
 
