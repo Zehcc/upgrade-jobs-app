ef8fedbcb4074be8ddda02918462bb8f06f36413
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-05-03 15:58:49 +0200

    chore: added company guide

---
+++ src/GuideData/data.js
@@ -0,0 +1,26 @@
+export const guideData = {
+    dataUser: [
+        {
+            title: '¡ El primer paso en tu búsqueda de empleo !',
+            text: 'Crea tu perfil profesional y sube tu CV a nuestra plataforma',
+        },
+        {
+            text: 'Empieza a aplicar a las ofertas que se adaptan a lo que estás buscando. Puedes filtrar por empresa o puesto',
+        },
+        {
+            text: '¡ Podrás ver el estado de tus candidaturas y el proceso en el que te encuentras dentro de cada empresa !',
+        },
+      ],
+      dataCompany: [
+        {
+            title: '¡Encuentra aqui los mejores candidatos para tus ofertas!',
+            text: 'Crea tus ofertas y deja que los candidatos se inscriban en ellas',
+        },
+        {
+            text: 'Evalúa cada candidato, pudiendo ver su perfil o leer su CV',
+        },
+        {
+            text: 'Si algún candidato cumple tus requisitos, habla con el directamente a través de nuestro chat',
+        },
+      ]
+}
--- src/components/UserGuide/UserGuide.jsx
+++ src/components/UserGuide/UserGuide.jsx
@@ -1,51 +1,115 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { guideData } from "../../GuideData/data";
 
 const GuideHome = () => {
   let navigate = useNavigate();
-  const dataText = [
-    {
-      title: '¡ El primer paso en tu búsqueda de empleo !',
-      text: 'Crea tu perfil profesional y sube tu CV a nuestra plataforma',
-    },
-    {
-      text: 'Empieza a aplicar a las ofertas que se adaptan a lo que estás buscando. Puedes filtrar por empresa o puesto',
-    },
-    {
-      text: '¡ Podrás ver el estado de tus candidaturas y el proceso en el que te encuentras dentro de cada empresa !',
-    },
-  ];
-  const [data, setData] = useState(0);
+  const { dataUser, dataCompany } = guideData;
+
+  const [data, setData] = useState(-1);
+  const [guide, setGuide] = useState("");
   const next = () => {
     setData(data + 1);
   };
-  const toRegisterUser = () => {
-    navigate('/registerUser');
+
+  const setUser = () => {
+    setGuide("user");
+    setData(data + 1);
   };
+
+  const setCompany = () => {
+    setGuide("company");
+    setData(data + 1);
+  };
+  const toRegister = (type) => {
+    if (type === "user") {
+      navigate("/registerUser");
+    } else {
+      navigate("/registerCompany");
+    }
+  };
+
   return (
-    <div className='guide'>
-      {data === 0 && <img className='guide__image' src='/assets/Resume.png' alt='profile' />}
-      {data === 1 && <img className='guide__image' src='/assets/Jobhunt.png' alt='profile' />}
-      {data === 2 && (
-        <img className='guide__image' src='/assets/contact-companies.png' alt='profile' />
+    <>
+      {data < 0 && (
+        <div>
+          <h2>¿Qué estás buscando?</h2>
+          <div>
+            <button onClick={setUser}>Estoy buscando trabajo</button>
+            <button onClick={setCompany}>Estoy buscando candidatos</button>
+          </div>
+        </div>
       )}
-      <div className='guide__content'>
-        {dataText[data].title && <h3>{dataText[data].title}</h3>}
-        <p>{dataText[data].text}</p>
-      </div>
-      <div className='guide__buttons'>
-        {data < 2 && (
-          <button className='guide-btn' onClick={next}>
-            Siguiente
-          </button>
+      <div className='guide'>
+        {data === 0 && (
+          <img
+            className='guide__image'
+            src='/assets/Resume.png'
+            alt='profile'
+          />
+        )}
+        {data === 1 && (
+          <img
+            className='guide__image'
+            src='/assets/Jobhunt.png'
+            alt='profile'
+          />
         )}
         {data === 2 && (
-          <button className='guide-btn green' onClick={toRegisterUser}>
-            Comenzar
-          </button>
+          <img
+            className='guide__image'
+            src='/assets/contact-companies.png'
+            alt='profile'
+          />
+        )}
+        {guide === "user" && (
+          <>
+            <div className='guide__content'>
+              {dataUser[data].title && <h3>{dataUser[data].title}</h3>}
+              <p>{dataUser[data].text}</p>
+            </div>
+            <div className='guide__buttons'>
+              {data < 2 && (
+                <button className='guide-btn' onClick={next}>
+                  Siguiente
+                </button>
+              )}
+              {data === 2 && (
+                <button
+                  className='guide-btn green'
+                  onClick={() => toRegister("user")}
+                >
+                  Comenzar
+                </button>
+              )}
+            </div>
+          </>
+        )}
+        {guide === "company" && (
+          <>
+            <div className='guide__content'>
+              {dataCompany[data].title && <h3>{dataCompany[data].title}</h3>}
+              <p>{dataCompany[data].text}</p>
+            </div>
+            <div className='guide__buttons'>
+              {data < 2 && (
+                <button className='guide-btn' onClick={next}>
+                  Siguiente
+                </button>
+              )}
+              {data === 2 && (
+                <button
+                  className='guide-btn green'
+                  onClick={() => toRegister("company")}
+                >
+                  Comenzar
+                </button>
+              )}
+            </div>
+          </>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
