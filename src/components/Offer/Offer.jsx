a861f188b6052184968d27f126cdefb27c6003ec
Tomás Sánchez <Tomassanchezmedina92@gmail.com>
2022-05-05 09:11:59 +0200

    chore: fixed offer

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Offer = ({ offer }) => {
-
   return (
     <Link to={`/detailedOffer/${offer._id}`}>
     <li className="offer-container">
@@ -12,7 +11,7 @@ const Offer = ({ offer }) => {
       <div className="offer-text-container">
         <h4>{offer.title}</h4>
         <p>{offer.company.name}</p>
-        <p>{offer.company.info.location}</p>
+        <p>{offer.location}</p>
       </div>
     </li>
     </Link>
